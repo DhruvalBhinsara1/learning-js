4b15ed6befd3321209623b43b1a0b9731cd771cd
Dhruval Bhinsara <48351115+DhruvalBhinsara1@users.noreply.github.com>
2025-03-26 16:34:35 +0530

    more changes

---
+++ LogicalOperators.js
@@ -0,0 +1,33 @@
+//Logical Operators
+
+// Logical AND (&&) operator returns true if both operands are true.
+
+//example:
+
+let a = true;
+let b = false;
+
+console.log(a && b); // false
+
+// Logical OR (||) operator returns true if either of the operands is true.
+
+//example:
+
+let c = true;
+let d = false;
+
+console.log(c || d); // true
+
+
+// Logical NOT (!) operator returns true if operand is false.
+
+//example:
+
+let e = true;
+
+console.log(!e); // false
+
+let f = false;
+
+console.log(!f); // true
+
