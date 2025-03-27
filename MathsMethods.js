6e559cb1b669d0eb2c8807b560b415da9b84cdc6
Dhruval Bhinsara <48351115+DhruvalBhinsara1@users.noreply.github.com>
2025-03-27 11:19:31 +0530

    Maths Methods

---
+++ MathsMethods.js
@@ -0,0 +1,50 @@
+//Built-in Math Functions
+
+//Math Properties (constants)
+
+//syntax
+
+//Math.property
+
+
+//Methods
+
+//number to integer
+
+//1.round (it returns the number to the nearest integer)
+
+//2.ceil (it reuturns rounded up to the nearest integer)
+
+//3.floor (it reuturns rounded down to the nearest integer)
+
+//4.trunc (returns the integer part of a number)
+
+//example
+
+//round 
+console.log()
+console.log("round")
+console.log(Math.round(4.7)); // Output: 5
+console.log(Math.round(5.2)) // Output : 5
+console.log(Math.round(5.5)); // Output : 6
+
+//ceil
+console.log()
+console.log("ceil")
+console.log(Math.ceil(4.7)); // Output: 5
+console.log(Math.ceil(5.2)); // Output : 6
+console.log(Math.ceil(5.5)); // Output : 6
+
+//floor
+console.log()
+console.log("floor")
+console.log(Math.floor(4.7)); // Output: 4
+console.log(Math.floor(5.2)); // Output : 5
+console.log(Math.floor(5.5)); // Output : 5
+
+//trunc
+console.log()
+console.log("trunc")
+console.log(Math.trunc(4.7)); // Output: 4
+console.log(Math.trunc(5.2)); // Output : 5
+console.log(Math.trunc(5.5)); // Output : 5
