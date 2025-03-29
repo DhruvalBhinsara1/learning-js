a90e3418f4d3f6377a8ef6f3caae1f7c7e237b27
Dhruval Bhinsara <48351115+DhruvalBhinsara1@users.noreply.github.com>
2025-03-29 13:51:03 +0530

    sadas

--- randomshit.js
+++ randomshit.js
@@ -1,3 +1,16 @@
-setInterval(() => {
-    console.log("Running indefinitely...");
-}, 500); // Runs every second
+fruit = "mango";
+
+switch (fruit) {
+    case "apple":
+        console.log("Apple is a fruit");
+        break;
+    case "mango":
+        console.log("Mango is a fruit");
+    case "banana":
+        console.log("Banana is a fruit");
+        break;
+    default:
+        console.log("We don't know that fruit");
+}
+
+//output will have mango and banana because of fall through
