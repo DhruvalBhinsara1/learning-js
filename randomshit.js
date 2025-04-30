dbe69675be39fca43bf3cb9b230ccab2dc6f147c
Dhruval Bhinsara <48351115+DhruvalBhinsara1@users.noreply.github.com>
2025-04-30 12:48:31 +0530

    Update randomshit.js

--- randomshit.js
+++ randomshit.js
@@ -1,16 +1,4 @@
-fruit = "mango";
-
-switch (fruit) {
-    case "apple":
-        console.log("Apple is a fruit");
-        break;
-    case "mango":
-        console.log("Mango is a fruit");
-    case "banana":
-        console.log("Banana is a fruit");
-        break;
-    default:
-        console.log("We don't know that fruit");
+for (let i = 0; i <= 5; i++) {
+    if (i % 2 == 0) continue;
+    console.log(i);
 }
-
-//output will have mango and banana because of fall through
